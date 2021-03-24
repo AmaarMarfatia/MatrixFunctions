4aaf1d388e129df1eb4d130062d1475f19c8996b
Amaar Marfatia <amaarmarfatia@gmail.com>
2021-03-24 15:39:45 -0400

    Fixed Add and Subtract Tests

--- matrixFuncs/test_add.py
+++ matrixFuncs/test_add.py
@@ -3,20 +3,23 @@ import unittest
 
 class TestAdd(unittest.TestCase):
 
-    def test_multi(self):
+    def test_2by2(self):
         a = [[1, 2], [4, 5]]
         b = [[1, 2], [3, 4]]
         c = add(a, b)
         self.assertEqual(c, [[2, 4], [7, 9]])
 
-        d = [[1,2],[3,4]]
-        e = [[5,6],[7,8]]
+    def test_3by3(self):
+        d = [[1,2,3],[4,5,6],[7,8,9]]
+        e = [[4,5,6],[7,8,9],[1,2,3]]
+        f = add(d,e)
+        self.assertEqual(f, [[5,7,9],[11,13,15],[8,10,12]])
 
-        f = [[1,2,3],[4,5,6],[7,8,9]]
-        g = [[4,5,6],[7,8,9],[1,2,3]]
-
+    def test_rectange(self):
+        g = [[1, 2, 3], [4, 5, 6]]
         h = [[1, 2, 3], [4, 5, 6]]
-        i = [[1, 2, 3], [4, 5, 6]]
+        i = add(g,h)
+        self.assertEqual(i, [[2,4,6],[8,10,12]])
 
 
 if __name__ == '__main__':
--- matrixFuncs/test_subtract.py
+++ matrixFuncs/test_subtract.py
@@ -3,11 +3,24 @@ import unittest
 
 class TestSubtract(unittest.TestCase):
 
-    def test_subtract(self):
+    def test_2by2(self):
         a = [[1, 2], [4, 5]]
         b = [[1, 2], [3, 4]]
         c = subtract(a, b)
         self.assertEqual(c, [[0, 0], [1, 1]])
+        
+    def test_3by3(self):
+        a = [[1,2,3],[4,5,6],[7,8,9]]
+        b = [[1,2,3],[6,5,4],[9,8,7]]
+        c = subtract(a, b)
+        self.assertEqual(c, [[0,0,0],[-2,0,2],[-2,0,2]])
+        
+    def test_rectange(self):
+        a = [[1, 2], [4, 5],[7,6]]
+        b = [[1, 2], [3, 4],[6,7]]
+        c = subtract(a, b)
+        self.assertEqual(c, [[0, 0], [1, 1],[1,-1]])
+        
 
 
 if __name__ == '__main__':
